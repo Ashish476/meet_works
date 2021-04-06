a6f38b024318cad71ab5ed1dddfc392c34753d0f
Ashish476 <59327202+Ashish476@users.noreply.github.com>
2021-04-06 11:40:15 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -6,14 +6,19 @@ const http = require('http')
 http.createServer((req, res) => {
         if(req.url === "/request"){
             apiCallFromRequest.callApi(function(response){
-     res.write(JSON.stringify(response));
-     res.end();
+                //console.log(JSON.stringify(response));
+                res.write(JSON.stringify(response));
+                res.end();
             });
         }
-
+        else if(req.url === "/node"){
+            apiCallFromNode.callApi(function(response){
+                res.write(response);
+                res.end();
+            });
+        }
         
-        
-        
+        // res.end();
 }).listen(3000);
 
 console.log("service running on 3000 port....");
